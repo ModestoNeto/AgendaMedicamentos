4307b760e32d26c2e7e07efb94eea59296c8e48d
Deivid Albuquerque <deivid.abq@gmail.com>
2025-11-01 16:44:22 -0300

    feat: adicionar novas páginas e atualizar estilos da aplicação

--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -2,16 +2,26 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Cadastro } from './pages/cadastro/cadastro';
+import { Agenda } from './pages/agenda/agenda';
+import { Medicamentos } from './pages/medicamentos/medicamentos';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {
     path: 'home',
     component: Home
+  },
+  {
+    path: 'agenda',
+    component: Agenda
+  },
+  {
+    path: 'medicamentos',
+    component: Medicamentos
   },
   {
     path: 'login',
